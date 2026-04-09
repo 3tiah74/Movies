9e077ef492bd072028d4b9edc9a3028c52f715ae
Abdulla Aly <Abdullah_20220285@fci.helwan.edu.eg>
2026-04-09 13:42:41 +0200

    Add admin management form to AdminDashboard with styling enhancements

--- FrontEnd/src/pages/Admin/AdminDashboard.jsx
+++ FrontEnd/src/pages/Admin/AdminDashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../../styles/AdminDashboard.css';
 import AdminLayout from '../../layouts/AdminLayout'
-import { Container, Row, Col, Card, Table } from 'react-bootstrap'
+import { Container, Row, Col, Card, Table, Form,Button } from 'react-bootstrap'
 
 const AdminDashboard = () => {
   return (
@@ -9,6 +9,7 @@ const AdminDashboard = () => {
       <AdminLayout>
         <div className='bg-dark'>
           <Container className='pt-5'>
+            <h1 className='text-white mb-4'>Welcome Abdulla</h1>
             <Row>
               <Col lg={4}>
                 <Card>
@@ -147,6 +148,25 @@ const AdminDashboard = () => {
                   </tr>
                 </tbody>
               </Table>
+            </Row>
+          </Container>
+          <Container className='pt-5'>
+            <Row>
+              <h2 className='text-white mb-4'>Add Admin</h2>
+              <Form className='d-flex gap-3 align-items-stretch w-100 ' >
+                <Form.Group className="flex-grow-1" controlId="formBasicName">
+                  <Form.Control className='control' type="text" placeholder="Enter name" />
+                </Form.Group>
+                <Form.Group className="flex-grow-1" controlId="formBasicEmail">
+                  <Form.Control className='control' type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group className="flex-grow-1" controlId="formBasicPassword">
+                  <Form.Control className='control' type="password" placeholder="Enter password" />
+                </Form.Group>
+                <Button variant='danger' type='submit' className='px-5 fw-bold'>
+                  Add Admin
+                </Button>
+              </Form>
             </Row>
           </Container>
         </div>
