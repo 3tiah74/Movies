0c6d3679236316d57e4e8211b6160d376a4ce8d4
Mostafa-Borham <borhamm56@gmail.com>
2026-04-30 16:22:13 +0300

    Delete src directory

--- src/main/java/com/movies/movies/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.movies.movies.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
