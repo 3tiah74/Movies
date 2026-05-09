76ac5051baf12384dfc8637a6a3eba2fa94bb10d
Tabar2004ak <87975346+Tabar2004ak@users.noreply.github.com>
2026-05-10 01:48:06 +0300

    Delete movies-service directory

--- movies-service/src/main/java/com/movies/movies/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.movies.movies.repository;
-
-import com.movies.movies.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
